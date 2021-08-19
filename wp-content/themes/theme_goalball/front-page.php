76b4df5394149c872789a261e592aaa9fd66cb77
tiavinaralandison@gmail.com <tiavinaralandison@gmail.com>
2021-08-19 11:46:24 +0200

    modification dans le front-page et content-front-page-panels

--- wp-content/themes/theme_goalball/front-page.php
+++ wp-content/themes/theme_goalball/front-page.php
@@ -18,18 +18,6 @@ get_header(); ?>
 	<main id="main" class="site-main" role="main">
 
 		<?php
-		// Show the selected front page content.
-/* 		if ( have_posts() ) :
-			while ( have_posts() ) :
-				the_post();
-				get_template_part( 'template-parts/page/content', 'front-page' );
-			endwhile;
-		else :
-			get_template_part( 'template-parts/post/content', 'none' );
-		endif; */
-		?>
-
-		<?php
 		// Get each of our panels and show the post data.
 		if ( 0 !== twentyseventeen_panel_count() || is_customize_preview() ) : // If we have pages to show.
 
